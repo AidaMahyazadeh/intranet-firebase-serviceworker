e5ace9155fdf3d849bbe90413ddb0f3616adb12d
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-12-29 09:39:12 +0100

    create costumDatasource

---
+++ frontend/src/app/shared/components/card-list/card-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
+})
+export class CardListComponent<T> implements OnInit{
+
+
+  ngOnInit(): void {
+    
+  }
+  
+
+
+
+
+}
