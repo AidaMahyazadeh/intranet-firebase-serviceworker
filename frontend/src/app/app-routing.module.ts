9cc0ed0b752768662167754b09abfb359127d037
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2024-01-16 17:05:28 +0100

    add route.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { adminAuthGuard } from './core/services/guards/admin-auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
   {path : '', redirectTo :'login',pathMatch:'full'}, 
+  {path :'home', component :HomeComponent},
   {path :'admin',canActivate :[adminAuthGuard] ,loadChildren:()=>import('./admin/admin.module').then(module=>module.AdminModule)},
   {
     path :'courses',loadChildren :()=>import('./courses/courses.module').then(module=>module.CoursesModule)
